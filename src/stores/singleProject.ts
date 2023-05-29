da39675018de0509d033b3ee9080f931a3e086a6
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2023-05-30 00:30:00 +0200

    [create-draft-project-model] Create draft project model

--- src/stores/singleProject.ts
+++ src/stores/singleProject.ts
@@ -8,6 +8,52 @@ const mockedSelectedProject = async (id: number) => {
         id,
         name: `Project ${id}`,
         description: `Description for project ${id}`,
+        columns: [
+          {
+            id: 1,
+            name: 'Column 1',
+            description: 'Description for column 1',
+            tasks: [
+              {
+                id: 1,
+                name: 'Task 1',
+                description: 'Description for task 1',
+              },
+              {
+                id: 2,
+                name: 'Task 2',
+                description: 'Description for task 2',
+              },
+              {
+                id: 3,
+                name: 'Task 3',
+                description: 'Description for task 3',
+              },
+            ],
+          },
+          {
+            id: 2,
+            name: 'Column 2',
+            description: 'Description for column 2',
+            tasks: [
+              {
+                id: 4,
+                name: 'Task 4',
+                description: 'Description for task 4',
+              },
+              {
+                id: 5,
+                name: 'Task 5',
+                description: 'Description for task 5',
+              },
+              {
+                id: 6,
+                name: 'Task 6',
+                description: 'Description for task 6',
+              },
+            ],
+          },
+        ],
       })
     }, 1000)
   })

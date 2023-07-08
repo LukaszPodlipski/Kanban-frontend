5920d49e75ac700f49b5cc21f300f6e8ac0720be
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2023-07-08 02:14:36 +0200

    [add-nested-snackbar-items] Add nested items in snackbar and logic

--- src/components/layout/sidebar/SideBar.vue
+++ src/components/layout/sidebar/SideBar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
+import { trimText } from '@/utils/functions'
 import { useRoute } from 'vue-router'
 import router from '@/router'
 import stores from '@/stores'
@@ -7,6 +8,8 @@ import stores from '@/stores'
 import { iProjectSimplified } from '@/types/projectsListTypes'
 
 import ArrowDownIcon from '@/components/icons/ArrowDownIcon.vue'
+import ArrowRightIcon from '@/components/icons/ArrowRightIcon.vue'
+import ArrowLeftIcon from '@/components/icons/ArrowLeftIcon.vue'
 import { iconComponent } from '@/composables/useDynamicIcon'
 
 const projectsStore = stores.useProjectsStore()
@@ -99,6 +102,14 @@ const menuItems = computed(
     ] as MenuItem[],
 )
 
+interface iNestedMenu {
+  prevRoute: string
+  name: string
+  items: MenuItem[]
+}
+
+const nestedMenu = ref<iNestedMenu | null>(null)
+
 const menuItemsStates = ref({
   Explore: false,
   Boards: false,
@@ -107,14 +118,8 @@ const menuItemsStates = ref({
 
 const fullSideBar = computed(() => layoutStore.sideBarOpened)
 
-const formatItemName = (name: string = ''): string => {
-  const trimName = (name: string = '') => {
-    if (name.length > 12) return name.slice(0, 12) + '...'
-    return name
-  }
-  return fullSideBar.value
-    ? trimName(name)
-    : name && name.slice(0, 1).toUpperCase()
+const formatItemName = (text: string = ''): string => {
+  return trimText(text, 11, fullSideBar.value)
 }
 
 const menuItemClasses = (menuItem: MenuItem, data: any = null) => {
@@ -166,6 +171,19 @@ const setActiveMenuItem = () => {
   )
   if (menuItem) toggleMenuItem(menuItem)
 }
+
+const setNestedMenu = (childItem: MenuItem, data: any) => {
+  nestedMenu.value = {
+    prevRoute: route.fullPath,
+    name: data.name,
+    items: childItem.nestedMenu as MenuItem[],
+  }
+}
+
+const clearNestedMenu = () => {
+  if (nestedMenu.value?.prevRoute) router.push(nestedMenu.value?.prevRoute)
+  nestedMenu.value = null
+}
 </script>
 
 <template>
@@ -175,6 +193,7 @@ const setActiveMenuItem = () => {
   >
     <div
       class="side-bar__logo flex align-items-center justify-content-center p-4"
+      :style="{ marginBottom: fullSideBar ? '0' : '2px' }"
     >
       <img
         src="../../../assets/icons/hamburger.svg"
@@ -189,13 +208,35 @@ const setActiveMenuItem = () => {
       >
     </div>
 
+    <div v-if="nestedMenu" class="flex flex-column">
+      <div
+        class="menu-item menu-item--rounded nested-menu-item pl-5 mr-5 pr-3"
+        :class="{ 'menu-item--mini': !fullSideBar }"
+      >
+        <ArrowLeftIcon :class="{'mr-3': fullSideBar}" @click="clearNestedMenu" />
+        <span v-if='fullSideBar'>{{ nestedMenu.name }}</span>
+      </div>
+      <div
+        v-for="menuItem in nestedMenu.items"
+        class="menu-item"
+        :class="menuItemClasses(menuItem)"
+        @click="executeMenuItemAction(menuItem)"
+      >
+        <div class="flex align-items-center">
+          <component :is="iconComponent(menuItem.icon)" class="icon" />
+          <span v-if="fullSideBar" class="menu-item__name">{{
+            formatItemName(menuItem.name)
+          }}</span>
+        </div>
+      </div>
+    </div>
     <div
+      v-else
       class="flex flex-column"
-      :style="{ marginTop: fullSideBar ? '0' : '2px' }"
     >
       <div v-for="item in menuItems" :key="item.name" id="menuItem">
         <div
-          class="menu-item flex align-items-center cursor-pointer"
+          class="menu-item"
           :class="{
             ...menuItemClasses(item),
             'justify-content-between': fullSideBar,
@@ -224,23 +265,32 @@ const setActiveMenuItem = () => {
         >
           <div
             v-if="child.multiple && child.items && child.items.length"
-            class="menu-item flex align-items-center cursor-pointer pl-6 pr-4 py-3 mr-4 pl-4"
+            class="menu-item pl-6 pr-4 py-3 mr-4"
             :class="menuItemClasses(child, childItem)"
             id="childRouteItem"
             v-for="(childItem, index) in child.items"
             :key="index"
             @click="executeMenuItemAction(child, childItem)"
           >
-            <div class="flex align-items-center">
-              <component :is="iconComponent(child.icon)" class="icon" />
-              <span class="menu-item__name">{{
-                formatItemName(childItem.name)
-              }}</span>
+            <div class="flex align-items-center justify-space-between">
+              <div class="flex align-items-center">
+                <component :is="iconComponent(child.icon)" class="icon" />
+                <span
+                  class="menu-item__name"
+                  :class="{ 'menu-item__name--mini': !fullSideBar }"
+                  >{{ formatItemName(childItem.name) }}</span
+                >
+              </div>
+              <ArrowRightIcon
+                v-if="fullSideBar"
+                class="menu-item__arrow-right ml-4"
+                @click="child.nestedMenu && setNestedMenu(child, childItem)"
+              />
             </div>
           </div>
           <div
             v-else
-            class="menu-item flex align-items-center cursor-pointer pl-6 pr-4 py-3 mr-4 pl-4"
+            class="menu-item pl-6 pr-2 py-3 mr-4"
             :id="child.action ? 'childActionItem' : 'childRouteItem'"
             :class="menuItemClasses(child)"
             @click="executeMenuItemAction(child)"
@@ -260,7 +310,8 @@ const setActiveMenuItem = () => {
 
 <style scoped lang="scss">
 .side-bar {
-  width: 300px;
+  width: 280px;
+  min-width: 280px;
   background-color: #2c2c38;
   border-top: 3px solid #2f2f3b;
   border-bottom: 3px solid #2f2f3b;
@@ -281,6 +332,18 @@ const setActiveMenuItem = () => {
 
   &--mini {
     width: 100px;
+    min-width: 100px;
+  }
+}
+
+.nested-menu-item {
+  background-color: #474479;
+  &:hover {
+    background-color: #474479 !important;
+  }
+
+  &:active {
+    color: #dfdcff !important;
   }
 }
 .menu-item {
@@ -289,12 +352,20 @@ const setActiveMenuItem = () => {
   color: #dfdcff;
   height: 48px;
   transition: transform 0.2s ease;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
 
   &__name {
+    width: 105px;
     margin-left: 16px;
     -webkit-user-select: none; /* Safari */
     -ms-user-select: none; /* IE 10 and IE 11 */
     user-select: none; /* Standard syntax */
+
+    &--mini {
+      width: auto;
+    }
   }
 
   &:hover {
@@ -365,6 +436,15 @@ const setActiveMenuItem = () => {
   }
   100% {
     transform: translateX(0);
+  }
+}
+
+@keyframes moveRight {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(10px);
   }
 }
 </style>
